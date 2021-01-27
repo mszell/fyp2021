bc250d8bf84d8aee04becae67de2ce67223adb8d
Michael Szell <michael.szell@gmail.com>
2021-01-27 17:21:52 +0100

    added lecture 4

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,17 +1,19 @@
-def restrict_dataset(dataset, indices, restrictfield = "Accident_Index"):
-	""" Restrict the data set dataset, given as structured array with an index field restrictfield
-	to the indices given in the dict indices. Returns the restricted data set.
+def barplot(data, field_name, field_categories):
+	"""Make a bar plot of a categorical variable, given as a field field_name in the
+	structured array data. Field categories and their names are givenin the dict field_categories.
 	"""
+	categories, counts = np.unique(data[field_name], return_counts=True)
 
-	# add later
-	#if type(indices) == "list": 
-	#	indices = {k: 0 for k in indices}
+	fig = plt.figure(figsize=(4, 3))
+	axes = fig.add_axes([0, 0, 1, 1]) # left, bottom, width, height (range 0 to 1)
+	axes.bar(range(len(categories)), counts, fc="gray") # fc is the face color
 
-	restrictedrowindices = []
-	for i in range(len(dataset[restrictfield])):
-	    if dataset[restrictfield][i] in indices:
-	        restrictedrowindices.append(i)
+	axes.set_xlabel("")
+	axes.set_ylabel('Count')
+	axes.set_title(field_name)
+	fig.autofmt_xdate(rotation=45)
 
-	return dataset[restrictedrowindices]
+	axes.set_xticks(range(len(categories)))
+	axes.set_xticklabels(field_categories.values());
 
 print("Loaded functions.\n")
